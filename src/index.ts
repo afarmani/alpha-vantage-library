b93151a9d6b44a9c068737118c00880f9de2dfb8
Amir Farmani <afarmani1a@gmail.com>
2020-12-24 18:44:38 -0500

    initialize NPM package

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+// export all the library parts to make available to consumer apps
+export {SymbolInfo} from './symbol-search/symbol-info';
+export {SymbolSearch} from './symbol-search/symbol-search';
---
+++ src/symbol-search/symbol-search.ts
@@ -0,0 +1,5 @@
+import {SymbolInfo} from "./symbol-info";
+
+export class SymbolSearch {
+  bestMatches: Array<SymbolInfo>;
+}
